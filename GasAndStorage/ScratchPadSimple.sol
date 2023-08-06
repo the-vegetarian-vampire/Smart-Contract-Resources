ee21cf4713ea7d13e1059082cf43c56d7ad108b0
John Lawniczak <105305546+the-vegetarian-vampire@users.noreply.github.com>
2023-08-06 14:19:08 -0400

    Add files via upload

---
+++ GasAndStorage/ScratchPadSimple.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract ScratchPadSimple {
+
+    // any state variable here
+    // mappings, arrays...
+
+   constructor() {
+       // set initial state
+   }
+
+    function current() external {
+        // logic for current function
+    }
+
+    function optimized() external {
+        // logic for optimized function
+    }
+}
